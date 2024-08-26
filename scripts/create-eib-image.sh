49e0a6dfd8270ede879962f1818b4bc7060910c7
Jiri Tomasek <jiri.tomasek@suse.com>
2024-08-26 06:51:49 -0400

    updates

--- scripts/create-eib-image.sh
+++ scripts/create-eib-image.sh
@@ -3,22 +3,35 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
-if [ $1 == "mgmt" ];then
-	# mgmt cluster
-	sudo podman run --rm --privileged -it -v ./eib-sno-rke-rancher:/eib \
-           registry.suse.com/edge/edge-image-builder:1.0.2 \
-           build --definition-file iso-definition.yaml
-elif [ $1 == "slm60" ];then
-	# mgmt cluster
-	sudo podman run --rm --privileged -it -v ./eib-sno-rke-rancher2.9.0:/eib \
-           registry.suse.com/edge/edge-image-builder:1.0.2 \
-           build --definition-file iso-definition.yaml
-elif [ $1 == "elemental-node" ];then
-	# edge cluster
-	sudo podman run --rm --privileged -it -v ./eib-elemental-image:/eib \
-           registry.suse.com/edge/edge-image-builder:1.0.2 \
-           build --definition-file iso-definition.yaml
-else
-	echo "error param: [mgmt|elemental-node|slm60] only supported"
-	exit 1
-fi
+# if [ $1 == "mgmt" ];then
+# 	# mgmt cluster
+# 	sudo podman run --rm --privileged -it -v ./eib-sno-rke-rancher:/eib \
+#            registry.suse.com/edge/edge-image-builder:1.0.2 \
+#            build --definition-file iso-definition.yaml
+# elif [ $1 == "slm60" ];then
+# 	sudo podman run --rm --privileged -it -v ./eib-sno-rke-rancher2.9.0:/eib \
+#            localhost/eib:dev \
+#            build --definition-file iso-definition.yaml
+# elif [ $1 == "elemental-node" ];then
+# 	# edge cluster
+# 	sudo podman run --rm --privileged -it -v ./eib-elemental-image:/eib \
+#            registry.suse.com/edge/edge-image-builder:1.0.2 \
+#            build --definition-file iso-definition.yaml
+# else
+# 	echo "error param: [mgmt|elemental-node|slm60] only supported"
+# 	exit 1
+# fi
+
+echo Building image from $1
+
+sudo podman run --rm --privileged -it -v $1:/eib \
+	registry.opensuse.org/isv/suse/edge/factory/test_manifest_images/edge-image-builder:1.2.0-rc1 \
+	build --definition-file iso-definition.yaml
+
+# sudo podman run --rm --privileged -it -v $1:/eib \
+# 	registry.suse.com/edge/3.1/edge-image-builder:1.1.0
+# 	build --definition-file iso-definition.yaml
+
+	# registry.opensuse.org/isv/suse/edge/factory/test_manifest_images/edge-image-builder:1.2.0-rc1 \
+
+# localhost/eib:dev \
--- scripts/create-vm-kcli.sh
+++ scripts/create-vm-kcli.sh
@@ -7,13 +7,55 @@ source ~/.bashrc
 # mgmt cluster machine
 # kcli create vm -P iso=jtomasek-eib-sno-rke-rancher-cluster.iso -P pool=VMs -P memory=32768 -P numcpus=8 -P disks=['{"size": 20, "interface": "sata"}'] -P nets=['{"name": "default", "mac": "aa:aa:aa:aa:aa:31"}]' jtomasek-mgmt-sno
 
-# rancher2..9.0 mgmt cluster machine
-kcli create vm -P iso=jtomasek-slm6.0-rke-rancher2.9.0-cluster.iso -P pool=VMs -P memory=32768 -P numcpus=8 -P disks=['{"size": 20, "interface": "sata"}'] -P nets=['{"name": "default", "mac": "aa:aa:aa:aa:aa:41"}]' jtomasek-mgmt-rancher2.9.0
+# rancher2.9.0 mgmt cluster machine
+# kcli create vm -P iso=jtomasek-slm6.0-rke-rancher2.9.2-cluster.iso -P pool=VMs -P memory=32768 -P numcpus=8 -P disks=['{"size": 50, "interface": "sata"}'] -P nets=['{"name": "default", "mac": "aa:aa:aa:aa:aa:32"}]' jtomasek-mgmt-rancher2.9.2
+
+# rancher2.10.0 mgmt cluster machine
+# kcli create vm \
+#   -P iso=jtomasek-slm6.0-rke2-rancher2.10-cluster.iso \
+#   -P pool=VMs \
+#   -P memory=32768 \
+#   -P numcpus=8 \
+#   -P disks=['{"size": 50, "interface": "sata"}'] \
+#   -P nets=['{"name": "default", "mac": "aa:aa:aa:aa:aa:22"}]' \
+#   jtomasek-mgmt-rancher2.10
+
+# edge-3.3
+kcli create vm \
+  -P iso=edge-3.3.0.iso \
+  -P pool=VMs \
+  -P memory=32768 \
+  -P numcpus=8 \
+  -P disks=['{"size": 50, "interface": "sata"}'] \
+  -P nets=['{"name": "default", "mac": "aa:aa:aa:aa:aa:82", "ip": "192.168.122.82", "reserveip": "true"}]' \
+  jtomasek-3.3.0-1
+
+# kcli create vm \
+#   -P iso=eib-mgmt-cluster-image-32.iso \
+#   -P pool=default \
+#   -P memory=162048 \
+#   -P numcpus=8 \
+#   -P disks=['{"size": 60, "interface": "sata"}'] \
+#   -P nets=['{"name": "default", "mac": "aa:aa:aa:bb:bb:22", "ip": "192.168.122.45", "reserveip": "true"}]' \
+#   prueba-alberto-default
+
+  # -P nets=['{"name": "default", "mac": "aa:aa:aa:aa:aa:70"}]' \
 
 # fedora40
 # curl -L https://download.fedoraproject.org/pub/fedora/linux/releases/40/Cloud/x86_64/images/Fedora-Cloud-Base-Generic.x86_64-40-1.14.qcow2 > ../VMs/fedora40.qcow2
 # kcli create vm -P image=fedora40.qcow2 -P pool=VMs -P memory=16384 -P numcpus=4 -P disks=['{"size": 10, "interface": "sata"}'] -P nets=['{"name": "default"}]' -P tpm=true -P uefi=true jtomasek-f
 
+# plain SL Micro 6
+# kcli create vm \
+#   -P iso=slm6-image.iso  \
+#   -P pool=VMs \
+#   -P memory=16384 \
+#   -P numcpus=4 \
+#   -P disks=['{"size": 10, "interface": "sata"}'] \
+#   -P nets=['{"name": "default"}]' \
+#   -P tpm=true \
+#   -P uefi=true \
+#   jtomasek-device6
 
 
 
